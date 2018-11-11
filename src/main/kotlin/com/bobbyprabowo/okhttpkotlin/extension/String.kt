30143a37855cc86fdd74d21fbea3be7aa9270f7d
bobby <prabowo.bobby@gmail.com>
2018-11-11 22:30:11 +0700

    initial commit

---
+++ src/main/kotlin/com/bobbyprabowo/okhttpkotlin/extension/String.kt
@@ -0,0 +1,22 @@
+package com.bobbyprabowo.okhttpkotlin.extension
+
+import okhttp3.*
+
+val JSON = MediaType.parse("application/json; charset=utf-8")
+
+fun String.httpGet(client: OkHttpClient): Call {
+    val request = Request.Builder()
+        .url(this)
+        .build()
+
+    return client.newCall(request)
+}
+
+fun String.httpPost(client: OkHttpClient, jsonString: String): Call {
+    val body = RequestBody.create(JSON, jsonString)
+    val request = Request.Builder()
+        .url(this)
+        .post(body)
+        .build()
+    return client.newCall(request)
+}
---
+++ src/test/kotlin/com/bobbyprabowo/okhttpkotlin/ExtensionTest.kt
@@ -0,0 +1,62 @@
+package com.bobbyprabowo.okhttpkotlin
+
+import com.bobbyprabowo.okhttpkotlin.extension.httpGet
+import com.bobbyprabowo.okhttpkotlin.extension.httpPost
+import okhttp3.OkHttpClient
+import okhttp3.mockwebserver.MockResponse
+import okhttp3.mockwebserver.MockWebServer
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertNotNull
+import org.junit.Test
+
+class ExtensionTest {
+
+    @Test
+    fun httpGet_shouldReturnCall() {
+
+        val baseUrl = "http://dummy.com/dumbCall"
+
+        val client = OkHttpClient()
+        val call = baseUrl.httpGet(client)
+        assertNotNull(call)
+    }
+
+
+    @Test
+    fun httpGet_shouldReturnCallThatCanPerformGetRequest() {
+
+        val path = "/dummy"
+        val expectedBody = "hello Dummy!!"
+        val server = MockWebServer()
+
+        // Schedule some responses.
+        server.enqueue(MockResponse().setBody(expectedBody))
+
+        // Start the server.
+        server.start()
+
+        // Ask the server for its URL. You'll need this to make HTTP requests.
+        val baseUrl = server.url(path)
+
+        val client = OkHttpClient()
+        val response = baseUrl.toString().httpGet(client).execute()
+        val bodyResult = response.body()?.string()
+        assertEquals(bodyResult, expectedBody)
+        server.shutdown()
+    }
+
+    @Test
+    fun httpPost_shouldReturnCall() {
+
+        val baseUrl = "http://dummy.com/dumbCall"
+        val jsonUrl = """"
+            {
+            name="hello"
+            }
+            """
+
+        val client = OkHttpClient()
+        val call = baseUrl.httpPost(client, jsonUrl)
+        assertNotNull(call)
+    }
+}
