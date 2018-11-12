5f5b8c634c3b81271d86f7869ca999ed39842708
bobby <prabowo.bobby@gmail.com>
2018-11-12 21:32:21 +0700

    complete unit test

--- src/test/kotlin/com/bobbyprabowo/okhttpkotlin/ExtensionTest.kt
+++ src/test/kotlin/com/bobbyprabowo/okhttpkotlin/ExtensionTest.kt
@@ -5,8 +5,7 @@ import com.bobbyprabowo.okhttpkotlin.extension.httpPost
 import okhttp3.OkHttpClient
 import okhttp3.mockwebserver.MockResponse
 import okhttp3.mockwebserver.MockWebServer
-import org.junit.Assert.assertEquals
-import org.junit.Assert.assertNotNull
+import org.junit.Assert.*
 import org.junit.Test
 
 class ExtensionTest {
@@ -34,6 +33,7 @@ class ExtensionTest {
     fun httpGet_shouldReturnCallThatCanPerformGetRequest() {
 
         val path = "/dummy"
+        val expectedRequestLine = "GET /dummy HTTP/1.1"
         val expectedBody = "hello Dummy!!"
         val server = MockWebServer()
 
@@ -45,10 +45,16 @@ class ExtensionTest {
 
         // Ask the server for its URL. You'll need this to make HTTP requests.
         val baseUrl = server.url(path)
+        val url = baseUrl.toString()
 
-        val response = baseUrl.toString().httpGet().execute()
+        val response = url.httpGet().execute()
         val bodyResult = response.body()?.string()
         assertEquals(bodyResult, expectedBody)
+
+        val request = server.takeRequest()
+        val resultRequestLine = request.requestLine
+        assertEquals(resultRequestLine, expectedRequestLine)
+
         server.shutdown()
     }
 
@@ -56,6 +62,7 @@ class ExtensionTest {
     fun httpGetWithClient_shouldReturnCallThatCanPerformGetRequest() {
 
         val path = "/dummy"
+        val expectedRequestLine = "GET /dummy HTTP/1.1"
         val expectedBody = "hello Dummy!!"
         val server = MockWebServer()
 
@@ -67,11 +74,16 @@ class ExtensionTest {
 
         // Ask the server for its URL. You'll need this to make HTTP requests.
         val baseUrl = server.url(path)
+        val url = baseUrl.toString()
 
         val client = OkHttpClient()
-        val response = baseUrl.toString().httpGet(client).execute()
+        val response = url.httpGet(client).execute()
         val bodyResult = response.body()?.string()
         assertEquals(bodyResult, expectedBody)
+
+        val request = server.takeRequest()
+        val resultRequestLine = request.requestLine
+        assertEquals(resultRequestLine, expectedRequestLine)
         server.shutdown()
     }
 
@@ -102,5 +114,78 @@ class ExtensionTest {
         val client = OkHttpClient()
         val call = baseUrl.httpPost(client, jsonBody)
         assertNotNull(call)
+    }
+
+    @Test
+    fun httpPost_shouldReturnCallThatCanPerformPostRequest() {
+
+        val path = "/dummy"
+        val expectedRequestLine = "POST /dummy HTTP/1.1"
+        val expectedBody = "hello Dummy!!"
+        val jsonBody = """"
+            {
+            name="hello"
+            }
+            """
+
+        val server = MockWebServer()
+
+        // Schedule some responses.
+        server.enqueue(MockResponse().setBody(expectedBody))
+
+        // Start the server.
+        server.start()
+
+        // Ask the server for its URL. You'll need this to make HTTP requests.
+        val baseUrl = server.url(path)
+        val url = baseUrl.toString()
+
+        val response = url.httpPost(jsonBody).execute()
+        val bodyResult = response.body()?.string()
+        assertEquals(bodyResult, expectedBody)
+
+        val request = server.takeRequest()
+        val resultRequestLine = request.requestLine
+        assertEquals(resultRequestLine, expectedRequestLine)
+        assertEquals("application/json; charset=utf-8", request.getHeader("Content-Type"));
+        assertEquals(jsonBody, request.body.readUtf8())
+        server.shutdown()
+    }
+
+    @Test
+    fun httpPostWithClient_shouldReturnCallThatCanPerformPostRequest() {
+
+        val path = "/dummy"
+        val expectedRequestLine = "POST /dummy HTTP/1.1"
+        val expectedBody = "hello Dummy!!"
+        val jsonBody = """"
+            {
+            name="hello"
+            }
+            """
+
+        val server = MockWebServer()
+
+        // Schedule some responses.
+        server.enqueue(MockResponse().setBody(expectedBody))
+
+        // Start the server.
+        server.start()
+
+        // Ask the server for its URL. You'll need this to make HTTP requests.
+        val baseUrl = server.url(path)
+        val url = baseUrl.toString()
+
+        val client = OkHttpClient()
+        val response = url.httpPost(client, jsonBody).execute()
+        val bodyResult = response.body()?.string()
+        assertEquals(bodyResult, expectedBody)
+
+        val request = server.takeRequest()
+        val resultRequestLine = request.requestLine
+        assertEquals(resultRequestLine, expectedRequestLine)
+        assertEquals("application/json; charset=utf-8", request.getHeader("Content-Type"));
+        assertEquals(jsonBody, request.body.readUtf8())
+        server.shutdown()
     }
 }
